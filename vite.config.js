d369a582d68a5d342b99097b082eafe0d915d8fe
Manuel Vázquez Acosta <manuel@merchise.org>
2023-07-03 14:01:45 +0200

    Fix the base of the build.

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import rescript from '@jihchi/vite-plugin-rescript';
 
 export default defineConfig({
   plugins: [rescript(),],
+  base: "",
+  build: ['es2020'],
   // Prevent ReScript messages from being lost when we run all things at the
   // same time.
   clearScreen: false,
